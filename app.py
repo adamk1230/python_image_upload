80a228d57aabb6e0574f9accbe2f90d8ef02525e
Adam Kwan <adamk1230@gmail.com>
2018-01-19 00:23:44 -0500

    made the image upload less janky

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from os.path import join, dirname, realpath
 from flask import Flask, render_template, redirect, url_for, request, send_from_directory
 from werkzeug.utils import secure_filename
 
+
 UPLOAD_FOLDER = 'static/UPLOAD_FOLDER'
 UPLOADS_PATH = join(dirname(realpath(__file__)), UPLOAD_FOLDER)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -19,6 +20,13 @@ def allowed_file(filename):
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
+    print("Request")
+    print(request)
+    print("Request Data")
+    print(request.data)
+    print("Request.files Here")
+    print(request.files)
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -31,19 +39,30 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            print("File begin: ------")
+            print(type(file))
+            print("File end: ------")
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             global image_object
             print("app")
             print(app.config['UPLOAD_FOLDER'], filename)
-            print("image path")
             path_split = image_path.split("static")
             path_string = "static" + path_split[1]
+            print("image path")
             print(path_string)
-            print()
+            print("url : ******")
+            print(request.url)
+            print("url for: ******")
+            uploads_url = url_for('uploaded_file',filename=filename)
+            uploads_url = uploads_url[1:]
+            print(uploads_url)
 
-            image_object = {"path": path_string}
+            img_src = request.url + uploads_url
+            print(img_src)
+
+            image_object = {"path": img_src}
             #image_object= { "path": url_for('uploaded_file')+"/"+filename }
             return redirect(url_for('upload_file'))
     return render_template("index.html", image = image_object)
@@ -51,6 +70,10 @@ def upload_file():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
+    print("send begin: ------")
+    #print(type((app.config['UPLOAD_FOLDER'], filename)))
+    print((app.config['UPLOAD_FOLDER'], filename))
+    print("send end: -------")
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
---
+++ test/test_basic.py
@@ -0,0 +1,40 @@
+import os
+import io
+import unittest
+
+from app import app
+
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_image_upload(self):
+        response = self.app.post('/', data=dict(file=(io.BytesIO(b"this is a test"), 'test.jpeg')), follow_redirects=True)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
